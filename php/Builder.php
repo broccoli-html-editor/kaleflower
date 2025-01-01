aac525683206e58e321f7177cb65cfcae2383584
Tomoya Koyanagi <tomk79@gmail.com>
2025-01-02 03:50:05 +0900

    buildメソッドをBuilderに移した

--- php/Builder.php
+++ php/Builder.php
@@ -20,6 +20,9 @@ class Builder {
 	/** $components */
 	private $components;
 
+	/** $fields */
+	private $fields;
+
 	/** $assets */
 	private $assets;
 
@@ -38,36 +41,12 @@ class Builder {
 	/**
 	 * Constructor
 	 */
-	public function __construct($utils, $config, $components, $assets){
+	public function __construct($utils){
 		$this->utils = $utils;
-		$this->config = $config;
-		$this->components = $components;
-		$this->assets = $assets;
 		$this->html = '';
 		$this->css = '';
 		$this->js = '';
 		$this->errors = array();
-	}
-
-	/**
-	 * Get HTML
-	 */
-	public function getHtml(){
-		return $this->html;
-	}
-
-	/**
-	 * Get CSS
-	 */
-	public function getCss(){
-		return $this->css;
-	}
-
-	/**
-	 * Get JS
-	 */
-	public function getJs(){
-		return $this->js;
 	}
 
 	/**
@@ -80,10 +59,137 @@ class Builder {
 	/**
 	 * Build
 	 * @param string $realpath_kflow Realpath of the kflow file.
-	 * @param array $options Build options
+	 * @param array $buildOptions Build options
 	 */
-	public function build( $content ){
-		$this->html .= $this->buildComponentsRecursive($content);
+	public function build( $realpath_kflow, $buildOptions = array() ){
+		$buildOptions = (is_array($buildOptions) || is_object($buildOptions) ? (object) $buildOptions : (object) array());
+		$buildOptions->assetsPrefix = $buildOptions->assetsPrefix ?? './';
+
+		$rtn = (object) array(
+			'result' => true,
+			'error' => null,
+			'html' => (object) array(
+				'main' => null,
+			),
+			'css' => null,
+			'js' => null,
+			'assets' => array(),
+		);
+		if( !is_file($realpath_kflow) || !is_readable($realpath_kflow) ){
+			return (object) array(
+				'result' => false,
+				'error' => 'kflow file is not exists or not readable.',
+				'html' => (object) array(),
+				'css' => null,
+				'js' => null,
+				'assets' => array(),
+			);
+		}
+
+		// DOMDocumentのインスタンス作成
+		$dom = new \DOMDocument();
+		$dom->load($realpath_kflow);
+
+		$xpath = new \DOMXPath($dom);
+		$configNodes = $xpath->query("/kflow/configs/config");
+		$styleNodes = $xpath->query("/kflow/styles/style");
+		$fieldNodes = $xpath->query("/kflow/fields/field");
+		$componentNodes = $xpath->query("/kflow/components/component");
+		$assetNodes = $xpath->query("/kflow/assets/asset");
+		$contentNodes = $xpath->query("/kflow/contents/content");
+
+		$this->config = (object) array(
+			'id' => null,
+		);
+		foreach ($configNodes as $configNode) {
+			$innerText = '';
+			$configName = $configNode->getAttribute('name');
+			$this->config->{$configName} = $configNode->getAttribute('value');
+		}
+
+		$this->fields = new Fields($this->utils);
+		foreach ($fieldNodes as $field) {
+			$this->fields->add_field($field);
+		}
+
+		$this->components = new Components($this->utils);
+		foreach ($componentNodes as $component) {
+			$this->components->add_component($component);
+		}
+
+		foreach ($styleNodes as $styleNode) {
+			$className = $styleNode->getAttribute('class');
+			$rtn->css .= '.'.$className.' {'."\n";
+			if( $styleNode->getAttribute('width') ){
+				$rtn->css .= '  width:'.$styleNode->getAttribute('width').';'."\n";
+			}
+			if( $styleNode->getAttribute('height') ){
+				$rtn->css .= '  height:'.$styleNode->getAttribute('height').';'."\n";
+			}
+			foreach ($styleNode->childNodes as $child) {
+				// 子ノードを文字列として追加
+				$rtn->css .= $styleNode->ownerDocument->saveHTML($child)."\n";
+			}
+			$rtn->css .= '}'."\n";
+		}
+
+		// アセット (返される用)
+		foreach ($assetNodes as $assetNode) {
+			$isPrivateMaterial = $assetNode->getAttribute('is-private-material');
+			if($this->utils->to_boolean($isPrivateMaterial)){
+				continue;
+			}
+			array_push($rtn->assets, (object) array(
+				'path' => $buildOptions->assetsPrefix.$assetNode->getAttribute('public-filename'),
+				'base64' => $assetNode->getAttribute('base64'),
+			));
+		}
+
+		// アセット (フィールドテンプレートに渡される用)
+		$this->assets = array();
+		foreach ($assetNodes as $assetNode) {
+			$assetId = $assetNode->getAttribute('id');
+			$this->assets[$assetId] = array(
+				'id' => $assetId,
+				'ext' => $assetNode->getAttribute('ext'),
+				'size' => intval($assetNode->getAttribute('size')),
+				'isPrivateMaterial' => $this->utils->to_boolean($assetNode->getAttribute('is-private-material')),
+				'path' => $buildOptions->assetsPrefix.$assetNode->getAttribute('public-filename'),
+				'base64' => $assetNode->getAttribute('base64'),
+				'field' => $assetNode->getAttribute('field'),
+				'fieldNote' => $assetNode->getAttribute('field-note'),
+			);
+		}
+
+		foreach ($contentNodes as $contentNode) {
+			$innerText = '';
+			$bowlName = $contentNode->getAttribute('name');
+			if(!strlen($bowlName ?? '')){
+				$bowlName = 'main';
+			}
+			foreach($contentNode->childNodes as $childNode){
+				$this->buildContent($childNode);
+
+				$rtn->html->{$bowlName} = $rtn->html->{$bowlName} ?? '';
+				$rtn->html->{$bowlName} .= $this->html;
+				$rtn->css .= $this->css;
+				$rtn->js .= $this->js;
+			}
+		}
+
+		return $rtn;
+	}
+
+	/**
+	 * Build Content
+	 * @param object $content Content object.
+	 */
+	private function buildContent( $content ){
+		$this->html = '';
+		$this->css = '';
+		$this->js = '';
+
+		$this->html = $this->buildComponentsRecursive($content);
 		return;
 	}
 
