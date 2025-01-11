e719082aaf8f7006982c82aadaf320ac5a6f34f6
Tomoya Koyanagi <tomk79@gmail.com>
2025-01-11 13:57:13 +0900

    LayoutViewにコンテンツが表示されるようになった。 (4)

--- src/kaleflower/includes/components/LayoutView/LayoutView_files/PreviewController.js
+++ src/kaleflower/includes/components/LayoutView/LayoutView_files/PreviewController.js
@@ -2,6 +2,9 @@ import utils79 from 'utils79';
 import previewContents from 'raw-loader!./previewContents.js';
 
 export class PreviewController {
+	#globalState;
+	#iframeElement;
+	#callbackMemory = {};
 
 	/**
 	 * Constructor
@@ -16,7 +19,9 @@ export class PreviewController {
 	 * @param {*} dist 
 	 */
 	async refresh (globalState, iframeElement, dist) {
-		const $ = globalState.jQuery;
+		this.#globalState = globalState;
+		this.#iframeElement = iframeElement;
+		const $ = this.#globalState.jQuery;
 		const $iframe = $(iframeElement);
 		const isPreviewStandby = iframeElement.getAttribute("data-kaleflower-preview-status");
 
@@ -36,7 +41,8 @@ export class PreviewController {
 			setTimeout(() => {
 				resolve();
 			}, 200);
-		}).then(() => { return new Promise((resolve) => {
+		})
+		.then(() => { return new Promise((resolve) => {
 			if(isPreviewStandby){
 				resolve();
 				return;
@@ -54,14 +60,53 @@ export class PreviewController {
 			event.origin = targetWindowOrigin;
 			win.dispatchEvent(event);
 
+			setTimeout(() => {
+				resolve();
+			}, 200);
+		});})
+		.then(() => { return new Promise((resolve) => {
+			if(isPreviewStandby){
+				resolve();
+				return;
+			}
+
+			this.#setupOnMessageEvent();
 			resolve();
-		});
-		}).then(() => { return new Promise((resolve) => {
+		});})
+		.then(() => { return new Promise((resolve, reject) => {
+			var timeout = setTimeout(() => {
+				console.error('postMessenger: ping error: Timeout');
+				reject();
+			}, 5000);
+			this.#send('ping', {}, (res) => {
+				try {
+					if( !res.result ){
+						console.error('postMessenger: ping got a error', res);
+					}
+
+				} catch(e) {
+					console.error('postMessenger: ping problem:', e);
+				}
+				clearTimeout(timeout);
+				resolve();
+			});
+		});})
+		.then(() => { return new Promise((resolve) => {
+
+			// スタンバイ完了を宣言する
 			$iframe.attr({"data-kaleflower-preview-status": true});
+
 			console.log('preview: refreshed');
 			resolve();
-		});
-		});
+		});});
+	}
+
+	/**
+	 * 新しいUUIDを生成する
+	 * @returns {String} UUID
+	 */
+	#createUUID(){
+		return "uuid-"+((new Date).getTime().toString(16)+Math.floor(1E7*Math.random()).toString(16));
 	}
 
 	/**
@@ -122,5 +167,77 @@ window.removeEventListener('message', f, false);
 </html>
 `;
 		return rtn;
+	}
+
+
+	/**
+	 * メッセージを送る
+	 */
+	#send(api, options, callback){
+		const iframe = this.#iframeElement;
+		const $ = this.#globalState.jQuery;
+		callback = callback||function(){};
+
+		var callbackId = this.#createUUID();
+
+		this.#callbackMemory[callbackId] = callback; // callbackは送信先から呼ばれる。
+
+		var message = {
+			'api': api,
+			'callback': callbackId,
+			'options': options
+		};
+
+		var win = $(iframe).get(0).contentWindow;
+		var targetWindowOrigin = this.#getTargetOrigin(iframe);
+
+		try {
+			var event;
+			event = document.createEvent("Event");
+			event.initEvent("message", false, false);
+			event.data = message;
+			event.origin = targetWindowOrigin;
+			win.dispatchEvent(event);
+		}catch(e){
+			win.postMessage(message, targetWindowOrigin);
+		}
+
+		return;
+	}
+
+	/**
+	 * メッセージを受信する
+	 */
+	#setupOnMessageEvent(){
+		const callbackMemory = this.#callbackMemory;
+		window.addEventListener('message',function(event){
+			var data=event.data;
+
+			if(data.api == 'unselectInstance'){
+				broccoli.unselectInstance();
+				return;
+
+			}else if(data.api == 'unfocusInstance'){
+				broccoli.unfocusInstance();
+				return;
+
+			}else if(data.api == 'onClickContentsLink'){
+				var data = event.data.options;
+				broccoli.options.onClickContentsLink(data.url, data);
+				return;
+
+			}else if(data.api == 'adjustPanelsPosition'){
+				broccoli.adjust();
+				return;
+
+			}else{
+				if(!callbackMemory[data.api]){return;}
+				callbackMemory[data.api](data.options);
+				callbackMemory[data.api] = undefined;
+				delete callbackMemory[data.api];
+			}
+			return;
+
+		});
 	}
 };
