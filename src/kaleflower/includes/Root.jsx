6db27c687451b08d1537d20454836081f3eba733
Tomoya Koyanagi <tomk79@gmail.com>
2025-04-15 21:57:12 +0900

    ビューポート幅の変更機能を追加した。 (2)

--- src/kaleflower/includes/Root.jsx
+++ src/kaleflower/includes/Root.jsx
@@ -24,6 +24,7 @@ const Root = React.memo((props) => {
 	globalState.utils = new Utils();
 	globalState.options = props.options;
 	globalState.lb = props.lb;
+	globalState.setGlobalState = setGlobalState;
 
 	useEffect(() => {
 		// カスタムイベントをリッスンしてデータを取得
@@ -193,89 +194,12 @@ const Root = React.memo((props) => {
 		);
 	}
 
-	// ブレイクポイントのサイズ配列を取得
-	const getBreakPointSizes = () => {
-		if (!globalState.configs || !globalState.configs['break-points']) {
-			return [];
-		}
-		
-		// 最大サイズを取得して100px大きいサイズも追加
-		const breakPoints = globalState.configs['break-points'];
-		const sizes = [];
-		let maxSize = 0;
-		
-		// breakPointsがオブジェクトの場合は配列に変換する
-		const breakPointsArray = Array.isArray(breakPoints) 
-			? breakPoints 
-			: Object.keys(breakPoints).map(key => breakPoints[key]);
-		
-		breakPointsArray.forEach(bp => {
-			const size = parseInt(bp['max-width'], 10);
-			sizes.push({
-				name: bp.name,
-				width: size
-			});
-			if (size > maxSize) {
-				maxSize = size;
-			}
-		});
-		
-		// 最大サイズよりも100px大きいサイズを追加
-		sizes.push({
-			name: 'xl',
-			width: maxSize + 100
-		});
-		
-		// サイズの小さい順にソート
-		return sizes.sort((a, b) => a.width - b.width);
-	};
-
-	// ビューポートサイズ変更ハンドラ
-	const handleViewportSizeChange = (size) => {
-		setGlobalState((prevState) => {
-			// 同じサイズが選択された場合は、成り行き幅にリセット
-			const newSize = prevState.selectedViewportSize === size ? null : size;
-			
-			return {
-				...prevState,
-				selectedViewportSize: newSize
-			};
-		});
-	};
-
-	// ビューポートサイズボタンをレンダリング
-	const renderViewportSizeButtons = () => {
-		const breakPointSizes = getBreakPointSizes();
-		
-		return (
-			<div className="kaleflower__header-viewport-tools">
-				<span className="kaleflower__header-viewport-tools-label">ビューポート:</span>
-				{breakPointSizes.map((size) => (
-					<button 
-						key={size.name}
-						className={`kaleflower__header-viewport-button ${globalState.selectedViewportSize === size.width ? 'kaleflower__header-viewport-button--active' : ''}`}
-						onClick={() => handleViewportSizeChange(size.width)}
-					>
-						{size.name} ({size.width}px)
-					</button>
-				))}
-				<button 
-					className={`kaleflower__header-viewport-button ${globalState.selectedViewportSize === null ? 'kaleflower__header-viewport-button--active' : ''}`}
-					onClick={() => handleViewportSizeChange(null)}
-				>
-					成り行き幅
-				</button>
-			</div>
-		);
-	};
-
 	return (
 		<MainContext.Provider value={globalState}>
 			<AppearanceStyles appearance={globalState.options.appearance} />
 			<div className="kaleflower__frame" onClick={unselectInstance}>
 				<div className="kaleflower__header">
 					<ToolBar />
-					{renderViewportSizeButtons()}
 				</div>
 
 				<div className="kaleflower__body">
